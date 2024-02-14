fef95cdcbf2e3ee733f95dede6f8b6ffe9abc07d
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-02-14 16:46:25 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const gifBear = document.getElementById("gifs");
 
     // Data and initial styles
-    const myList = ["Sure?", "Are you really sure??", "Please?", "Pretty please?", "I will be sad", "I will be really sad", "Come on!"];
+    const myList = ["Sure?", "Are you really sure??", "Please?", "Pretty please?", "I will be sad 宝贝", "I will be really sad", "Come on!"];
     let yesWidth = 250;
     let yesHeight = 115;
     let yesPadding = 20;
     let size = 50;
     let padding = 25;
+    let font = 20;
 
     // Function for "No" button click event
     function noBtnClickEvent(obj) {
@@ -21,20 +22,23 @@ document.addEventListener('DOMContentLoaded', function() {
             obj.innerHTML = myList[randomNum];
             yesButton.style.width = `${yesWidth + size}px`;
             yesButton.style.height = `${yesHeight + size}px`;
-            yesButton.style.paddingTop = `${yesPadding + padding}px`;
+            yesButton.style.padding = `${yesPadding + padding}px`;
+            yesButton.style.fontSize = `${font}px`;
         }
         size += 50;
         padding += 25;
+        font += 6;
     }
 
     // Function for "YES" button click event
     function yesBtnClickEvent() {
-        gifBear.src = "bear-kiss-bear-kisses.gif"; // Make sure the path is correct
+        gifBear.src = "bear-kiss-bear-kisses.gif"; 
         yesButton.style.width = `${yesWidth}px`;
         yesButton.style.height = `${yesHeight}px`;
-        yesButton.style.paddingTop = `${yesPadding}px`;
-        questionStr.innerHTML = "Ok Yaaaay !!!";
-        noButton.innerHTML = "I knew it !";
+        yesButton.style.padding = `${yesPadding}px`;
+        yesButton.style.fontSize = `${sizeFont}px`;
+        questionStr.innerHTML = "Oh Yaaaay!!!";
+        noButton.innerHTML = "我知道了 !";
     }
 
     // Attach event listeners
